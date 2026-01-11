c69848afc8d5d3a0e9d05c3c92d6dcd4e266c335
sofimes <sofanitmesfin19@gmail.com>
2026-01-11 12:11:19 +0300

    class solution added

--- java/leet190.java
+++ java/leet190.java
@@ -11,7 +11,7 @@ public class leet190 {
         System.out.println("Reversed: " + reversed);
     }
 }
-
+// solution class
 class Solution {
     public int reverseBits(int n) {
         int result = 0;
